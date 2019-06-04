57e5347b5dad0653ca1345478a3ca6f37740e41d
yangxiuchu <18487304527@163.com>
2019-06-04 14:27:19 +0800

    分布式配置、服务调用追踪。

---
+++ springcloud/cluster-config-server/src/main/java/com/forezp/ccs/ClusterConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.forezp.ccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class ClusterConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterConfigServerApplication.class, args);
+	}
+}
