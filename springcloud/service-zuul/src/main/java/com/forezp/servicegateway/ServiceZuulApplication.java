1e1cd916794de12e982f76f8d424dded4f6f82ad
yangxiuchu <18487304527@163.com>
2019-06-05 16:36:44 +0800

    Spring Cloud Gateway 初体验。

--- springcloud/service-zuul/src/main/java/com/forezp/servicegateway/ServiceZuulApplication.java
+++ springcloud/service-zuul/src/main/java/com/forezp/servicegateway/ServiceZuulApplication.java
@@ -1,4 +1,4 @@
-package com.forezp.servicezuul;
+package com.forezp.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springcloud/service-zuul/src/main/java/com/forezp/servicegateway/filter/MyFilter.java
+++ springcloud/service-zuul/src/main/java/com/forezp/servicegateway/filter/MyFilter.java
@@ -1,4 +1,4 @@
-package com.forezp.servicezuul.filter;
+package com.forezp.servicegateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
