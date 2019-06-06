85f26ee0d601555cbb109ff5b763a234a4d4e0f2
yangxiuchu <18487304527@163.com>
2019-06-06 23:09:28 +0800

    Spring Cloud Filter

---
+++ springcloud/gateway/service-gateway-filter/src/main/java/com/forezp/sgp/ServiceGatewayFilterApplication.java
@@ -0,0 +1,45 @@
+package com.forezp.sgp;
+
+import com.forezp.sgp.factory.RequestTimeGatewayFilterFactory;
+import com.forezp.sgp.filter.RequestTimeFilter;
+import com.forezp.sgp.filter.TokenFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServiceGatewayFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayFilterApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        // @formatter:off
+        return builder.routes()
+                .route(r -> r.path("/customer/**")
+                        .filters(f -> f.filter(new RequestTimeFilter())
+                                .addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+                        .uri("http://httpbin.org:80/get")
+                        .order(0)
+                        .id("customer_filter_router")
+                )
+                .build();
+        // @formatter:on
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory elapsedGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+}
